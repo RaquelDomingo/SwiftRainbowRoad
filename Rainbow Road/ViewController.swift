b1f24f305c9befc7983d972c052fc0941c816e19
Raquel Domingo <raqueldomingo@Raquel-Domingo.local>
2017-10-22 00:20:36 -0700

    Initial commit

---
+++ Rainbow Road/ViewController.swift
@@ -0,0 +1,49 @@
+//
+//  ViewController.swift
+//  Rainbow Road
+//
+//  Created by Raquel Domingo on 9/11/17.
+//  Copyright © 2017 Raquel Domingo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate{
+    
+    var colors = [UIColor.red, UIColor.orange, UIColor.yellow, UIColor.green, UIColor.blue, UIColor.purple]
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDataSource{
+
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 6
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.backgroundColor = colors[indexPath.row]
+        return cell
+    }
+}
+
+
+
